a7024a111a348a46b4acb74b0355a8dce4165de9
CmouseG <1668924806@qq.com>
2017-09-30 21:13:13 -0700

    import cicada-framework-vue

--- framework/home/src/main.js
+++ framework/home/src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Cicada from 'cicada-framework-vue'
 import App from './App'
 import router from './router'
+
+console.log(Cicada)
+Vue.use(Cicada)
 
 Vue.config.productionTip = false
 
